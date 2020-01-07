0a521127ec828b7de995b80e133d68b0f61e642e
Noah Maizels <noahmaizels@hotmail.com>
2020-01-07 23:05:55 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, NavLink, Route, Switch } from 'react-router-dom'
 import Home from './pages/home'
 import BetaWallet from './pages/betawallet'
 import LightWallet from './pages/lightwallet'
 import MyWanWallet from './pages/mywanwallet'
 import logo from './images/wanchain-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      menuDisplay: false,
+    }
+  }
+  burgerBarHandler = () => {
+    this.setState({menuDisplay: !this.state.menuDisplay})
+  }
+  render() {
+    const activeNavStyle = {color: "rgb(0,165,255)", textDecoration: "underline",  marginRight: "30px"}
+    const navStyle = {fontSize: "1.8rem", fontWeight: 700, color: "white", textDecoration: "none",  marginRight: "30px" }
+    const menuVis = this.state.menuDisplay ? 'show' : 'hide'
+    console.log(menuVis)
+    return (
+      <Router>
+        <div className="header-background"></div>
+   
+        <nav className="navbar">
+          <img className ="logo" src={logo} alt="logo"/>
+          <span onClick={this.burgerBarHandler} className="burger-bar">
+            <FontAwesomeIcon  icon={faBars} size="3x" color="green"/>
+          </span>
+          <div className={`flex-container ${menuVis}`}>
+            <NavLink className="NavLink" exact to="/" style={navStyle} activeStyle={activeNavStyle}>Home</NavLink>
+            <NavLink className="NavLink" to="/beta-wallet/" style={navStyle} activeStyle={activeNavStyle}>Beta Wallet</NavLink>
+            <NavLink className="NavLink" to="/light-wallet/" style={navStyle} activeStyle={activeNavStyle}>Light Wallet</NavLink>
+            <NavLink className="NavLink" to="/my-wan-wallet/" style={navStyle} activeStyle={activeNavStyle}>MyWanWallet</NavLink>
+          </div>
+        </nav>
 
-function App() {
- 
-  const activeNavStyle = {color: "rgb(0,165,255)", textDecoration: "underline",  marginRight: "30px"}
-  const navStyle = {fontSize: "1.5rem", color: "white", textDecoration: "none",  marginRight: "30px" }
-
-  return (
-        <Router>
-          <img class ="logo" src={logo} alt="logo"/>
-          <nav className="navbar">
-            <Link class="Link" exact to="/" style={navStyle} activeStyle={activeNavStyle}>Home</Link>
-            <Link class="Link" to="/beta-wallet/" style={navStyle} activeStyle={activeNavStyle}>Beta Wallet</Link>
-            <Link class="Link" to="/light-wallet/" style={navStyle} activeStyle={activeNavStyle}>Light Wallet</Link>
-            <Link class="Link" to="/my-wan-wallet/" style={navStyle} activeStyle={activeNavStyle}>MyWanWallet</Link>
-          </nav>
-
-          <Switch>
-            <Route path="/beta-wallet/" component={BetaWallet}/>
-            <Route path="/light-wallet/" component={LightWallet}/>
-            <Route path="/my-wan-wallet/" component={MyWanWallet}/>
-            <Route path="/" component={Home}/>
-          </Switch>
-        </Router>
-  );
+        <Switch>
+          <Route path="/beta-wallet/" component={BetaWallet}/>
+          <Route path="/light-wallet/" component={LightWallet}/>
+          <Route path="/my-wan-wallet/" component={MyWanWallet}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Router>
+);
+  }
 }
 
 export default App;
